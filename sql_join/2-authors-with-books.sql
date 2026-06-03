b29e0eba4d51e503c6652242cf9f5424c00ea25a
sagalou <sagal.louise.haider@gmail.com>
2026-06-03 12:39:10 +0200

    add file 2-authors-with-books.sql

--- sql_join/2-authors-with-books.sql
+++ sql_join/2-authors-with-books.sql
@@ -1,4 +1,4 @@
 SELECT authors.name AS author_name, books.title  
 FROM authors
-INNER JOIN books ON books.author_id = authors.id
+LEFT OUTER JOIN books ON books.author_id = authors.id
 ORDER BY author_name, title;
