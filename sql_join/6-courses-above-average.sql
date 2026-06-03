3dea549c4953069b51b43a4dc969b8d605daac11
sagalou <sagal.louise.haider@gmail.com>
2026-06-03 14:13:11 +0200

    fix file 6-courses-above-average.sql

--- sql_join/6-courses-above-average.sql
+++ sql_join/6-courses-above-average.sql
@@ -1,6 +1,6 @@
 SELECT courses.title AS course_title
 FROM courses
-INNER JOIN enrollments ON course_id = enrollments.course_id
+INNER JOIN enrollments ON courses.id = enrollments.course_id
 GROUP BY courses.title
 HAVING COUNT(*) > (
     SELECT AVG(cnt) FROM (SELECT COUNT(*) AS cnt FROM enrollments GROUP BY course_id)
